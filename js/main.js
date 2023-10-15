1d5319c9ac50426ada6f2dc1eb3a3f8df4e9aad9
AndreyP <mranderteyker@gmail.com>
2023-10-16 00:17:16 +0300

    123

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=> {
   menuMobile.classList.toggle('menu--open');
 });
+
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+ 
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
